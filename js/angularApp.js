7b58a0fb870db372487f21977c9760303740b143
odparraj <osdario17@gmail.com>
2017-08-19 18:40:09 -0500

    new changes

--- js/angularApp.js
+++ js/angularApp.js
@@ -3,6 +3,7 @@ var app = angular.module("app", []);
 app.controller("appCtrl", function($scope) {
 
 	$scope.lang= 'en';
+    $scope.date= new Date();
 
     $scope.translate = {
     	es: {
@@ -10,6 +11,7 @@ app.controller("appCtrl", function($scope) {
     		txt_about: 'Quienes somos',
     		txt_services: 'Servicios',
     		txt_clients: 'Clientes',
+            txt_our_clients: 'Nuestros clientes',
     		txt_contact: 'Contacto',
 
     		txt_about_content: 'Know How Asesores nace de la necesidad de fortalecer el desarrollo de organizaciones privadas y públicas  a través de asesorías ligadas a un adecuado acompañamiento durante la implementación de las estrategias diseñadas. Esta iniciativa se sustenta además al observar la evidente insostenibilidad y difícil crecimiento de las mipymes en Colombia, las cuales en su mayoría, llegan a ser liquidadas y/o abandonadas al cumplir en promedio los dos años de existencia. Esto a pesar que nuestro país, ha logrado ubicarse en las primeras posiciones en materia de creación de empresas a nivel de América Latina durante los últimos años.',
@@ -17,7 +19,60 @@ app.controller("appCtrl", function($scope) {
     		txt_mission: 'Misión',
     		txt_mission_content: 'Nos mueve ser parte del fortalecimiento y crecimiento de las entidades públicas y privadas, y más aún del desarrollo de la fuerza emprendedora, lo cual se traduce en un fundamento clave para el progreso sostenible de la sociedad actual.',
     		txt_vision: 'Visión',
-    		txt_vision_content: 'A 2020 seremos empresa líder a nivel nacional, reconocida por su excelente contribución al desarrollo, fortalecimiento y crecimiento de las organizaciones, fundamentada en un capital humano competente y brindando servicios con los más altos estándares de calidad. Así mismo, y además del mercado norteamericano, cubriremos importantes consultorías en el resto de América Latina.'
+    		txt_vision_content: 'A 2020 seremos empresa líder a nivel nacional, reconocida por su excelente contribución al desarrollo, fortalecimiento y crecimiento de las organizaciones, fundamentada en un capital humano competente y brindando servicios con los más altos estándares de calidad. Así mismo, y además del mercado norteamericano, cubriremos importantes consultorías en el resto de América Latina.',
+    		txt_management_consulting: 'Consultoría de gestión',
+    		txt_economic_and_business_consulting:'Consultoría económica y empresarial',
+    		txt_process_improvement: 'Mejora de procesos',
+    		txt_human_management: 'Gestión humana',
+    		txt_management_systems: 'Sistemas de gestión',
+
+            array_process_improvement: [
+        		'Diagnóstico o evaluación de Satisfacción del cliente.',
+        		'Análisis y optimización de procesos.',
+        		'Lean Production.',
+        		'6 Sigma.',
+        		'AMFE - Análisis Modal de Fallos y Efectos.',
+        		'Gestión de datos e indicadores.'
+            ],
+
+            array_human_management: [
+        		'Evaluación del clima, microclima y cultura organizacional.',
+        		'Coaching empresarial.',
+        		'Coaching organizacional.',
+        		'Desarrollo de Equipos de Alto Rendimiento.',
+        		'Diseño de modelos para medir competencias funcionales y comportamentales.',
+        		'Planes de intervención de Clima y Cultura Organizacional.'
+            ],
+
+
+            array_management_systems: [
+        		'Implementación ISO 9001 - Gestión de la Calidad.',
+    			'Implementación ISO 22000 - Gestión de la Calidad en Empresas Alimentarias.',
+    			'Implementación ISO 17025 - Gestión de la calidad en Laboratorios.',
+    			'Implementación ISO 27001 - Gestión de la seguridad de la información',
+    			'Implementación ISO 14001 - Gestión Medioambiental.',
+    			'Implementación SA 8000 - Responsabilidad Social Corporativa.',
+    			'Implementación OHSAS 18001 - Gestión de la Prevención.',
+    			'Elaboración de Reportes de Sostenibilidad bajo Metodología GRI Standards.'
+            ],
+
+            array_economic_and_business_consulting: [
+    			'Estudios de factibilidad.',
+    			'Planes de negocio.',
+    			'Investigación de mercados nacionales e internacionales.',
+    			'Análisis de impactos.',
+    			'Diagnóstico DAFO y BENCHMARKING.',
+    			'Despliegue estratégico: Cuadro de Mando Integral (CMI).',
+    			'Diseño de la planeación estratégica.',
+    			'Elaboración de proyectos de iniciativa pública y privada.',
+    			'Estudio de oferta y demanda para el ajuste de tarifas para el sector transporte y servicios públicos.'
+            ],
+
+            form_txt_email: 'Email personal',
+            form_txt_name: 'Nombre completo',
+            form_txt_subject: 'Asunto',
+            form_txt_message: 'Mensaje',
+            form_text_send: 'Enviar'
     	},
 
     	en: {
@@ -25,6 +80,7 @@ app.controller("appCtrl", function($scope) {
     		txt_about: 'About us',
     		txt_services: 'Services',
     		txt_clients: 'Customers',
+            txt_our_clients: 'Our clients',
     		txt_contact: 'Contact',
 
     		txt_about_content: 'Know How Advisors arises from the need to strengthen the development of private and public organizations through advisory services linked to an adequate accompaniment during the implementation of the strategies designed. This initiative is also sustained by observing the evident unsustainability and difficult growth of microenterprises in Colombia, which are mostly liquidated and / or abandoned on average in the two years of existence. This in spite of our country, has managed to position itself in the top positions in business creation in Latin America in recent years.',
@@ -32,12 +88,62 @@ app.controller("appCtrl", function($scope) {
     		txt_mission: 'Mission',
     		txt_mission_content: 'It moves us to be part of the strengthening and growth of public and private entities, and even more of the development of the entrepreneurial force, which translates into a key foundation for the sustainable progress of today\'s society.',
     		txt_vision: 'Vision',
-    		txt_vision_content: 'By 2020 we will be a leading company at the national level, recognized for its excellent contribution to the development, strengthening and growth of organizations, based on competent human capital and providing services with the highest quality standards. Likewise, besides the North American market, we will cover important consultancies in the rest of Latin America.'
+    		txt_vision_content: 'By 2020 we will be a leading company at the national level, recognized for its excellent contribution to the development, strengthening and growth of organizations, based on competent human capital and providing services with the highest quality standards. Likewise, besides the North American market, we will cover important consultancies in the rest of Latin America.',
+    		txt_management_consulting: 'Management consulting',
+    		txt_economic_and_business_consulting:'Economic and business consulting',
+    		txt_process_improvement: 'Process improvement',
+    		txt_human_management: 'Human management',
+    		txt_management_systems: 'Management systems',
+
+            array_process_improvement:[
+        		'Diagnosis or evaluation of customer satisfaction.',
+        		'Analysis and optimization of processes.',
+        		'Lean Production.',
+        		'6 Sigma.',
+        		'AMFE - Modal Analysis of Faults and Effects.',
+        		'Data management and indicators.'
+            ],
+
+            array_human_management: [            
+        		'Evaluation of the climate, microclimate and organizational culture.',
+        		'Business coaching.',
+        		'Organizational Coaching',
+        		'Development of High Performance Equipment.',
+        		'Design of models to measure functional and behavioral competences',
+        		'Climate and Organizational Culture intervention plans.'
+            ],
+
+            array_management_systems: [
+        		'Implementation ISO 9001 - Quality Management.',
+    			'Implementation ISO 22000 - Management of Quality in Food Companies.',
+    			'Implementation ISO 17025 - Quality Management in Laboratories.',
+    			'Implementation ISO 27001 - Management of information security.',
+    			'Implementation ISO 14001 - Environmental Management.',
+    			'Implementation SA 8000 - Corporate Social Responsibility.',
+    			'Implementation OHSAS 18001 - Management of Prevention.',
+    			'Elaboration of Sustainability Reports under GRI Standards Methodology.'            
+            ],
+
+            array_economic_and_business_consulting: [
+    			'Feasibility studies.',
+    			'Business plans.',
+    			'Research of national and international markets.',
+    			'Impact analysis.',
+    			'Diagnosis DAFO and BENCHMARKING.',
+    			'Strategic Deployment: Integral Command Chart (CMI).',
+    			'Design of strategic planning.',
+    			'Elaboration of projects of public and private initiative.',
+    			'Supply and demand study for the adjustment of tariffs for the transport and public services sector.'
+            ],
+
+            form_txt_email: 'Your Email',
+            form_txt_name: 'Your Name',
+            form_txt_subject: 'Subject',
+            form_txt_message: 'Message',
+            form_text_send: 'Send'
+
+
     	}
     };
-
-    
-
-
 
 });
